e7d23ed13d5f79d9bd3ea10773c89e8b926e4c5a
thales <thalestr@outlook.com>
2023-01-31 20:45:12 -0300

    first commit

---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+  public Transform border_top;
+  public Transform border_bottom;
+  public Transform border_left;
+  public Transform border_right;
+
+  public GameObject prefabFood;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    InvokeRepeating("Spawn", 3, 4);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+
+  void Spawn()
+  {
+    int x = (int)Random.Range(border_left.position.x, border_right.position.x);
+    int y = (int)Random.Range(border_top.position.y, border_bottom.position.y);
+
+    Instantiate(prefabFood, new Vector2(x, y), Quaternion.identity);
+  }
+}
